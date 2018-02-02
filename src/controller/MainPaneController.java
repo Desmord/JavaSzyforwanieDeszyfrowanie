c24d22ac2c8e26db09d4ffcb750db5d005c1a53f
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-02-02 18:17:22 +0100

    Dodanie wszystkich elementow widoku

--- src/controller/MainPaneController.java
+++ src/controller/MainPaneController.java
@@ -3,9 +3,30 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class MainPaneController implements Initializable {
+
+	@FXML
+	private ChoiceBox<?> encriptionChoiseBox;
+
+	@FXML
+	private TextArea codeTextField;
+
+	@FXML
+	private Label actionTitleLabel;
+
+	@FXML
+	private Button encryptButton;
+
+	@FXML
+	private Button decryptButton;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		System.out.println("Witam");
