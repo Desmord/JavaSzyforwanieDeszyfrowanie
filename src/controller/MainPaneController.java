3c6ccb685f6a89f7fc520ed4e0ecaf73099d433e
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-02-02 18:22:33 +0100

    Dodanie probnych zdazen przyciskow

--- src/controller/MainPaneController.java
+++ src/controller/MainPaneController.java
@@ -3,12 +3,16 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 
 public class MainPaneController implements Initializable {
 
@@ -30,5 +34,22 @@ public class MainPaneController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		System.out.println("Witam");
+		proba();
+		proba2();
+	}
+	
+	private void proba2() {
+		 decryptButton.addEventFilter(MouseEvent.MOUSE_CLICKED, x-> {
+			 codeTextField.setText("de witam");
+		 });
+	}
+	
+	private void proba() {
+		encryptButton.setOnAction(new EventHandler<ActionEvent>() {
+			 @Override
+		        public void handle(ActionEvent event) {
+		        	codeTextField.setText("Witam");
+		        }
+		});
 	}
 }
